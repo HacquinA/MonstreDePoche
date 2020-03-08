7ef7e5aa1039af377820a118d63a4bd2e2b3ddfb
HacquinA <athenais.hacquin@gmail.com>
2020-03-08 18:12:07 +0100

    affichage
    affichage ameliorer

---
+++ Perso.cpp
@@ -0,0 +1,14 @@
+#include "Perso.h"
+
+myTeam::myTeam(); vie(50), attaque1(10), attaque2(20);
+
+void getVie(){
+    return vie;
+}
+
+void setVie(int vie){
+    vie += vie;
+}
+
+enemie::enemie(); vie(50), attaque1(10), attaque2(20);
+
---
+++ Perso.h
@@ -0,0 +1,29 @@
+using namespace std;
+
+class myTeam{
+    private:
+        int vie;
+        int attaque1;
+        int attaque2;
+        int attaque3;
+        int attaque4;
+    public:
+        int getVie();
+        int getAttaque();
+        void setPv(int);
+        myTeam();
+};
+
+class enemie{
+    private:
+        int vie;
+        int attaque1;
+        int attaque2;
+        int attaque3;
+        int attaque4;
+    public:
+        int getVie();
+        int getAttaque();
+        void setPv(int);
+        enemie();
+};
