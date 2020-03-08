73766dae2a2d82382a322bf252e4cf257ddde5cb
HacquinA <athenais.hacquin@gmail.com>
2020-03-08 17:49:02 +0100

    attaque et ennemie
    ajout de l'attaque des deux cotes

--- Perso.cpp
+++ Perso.cpp
@@ -5,3 +5,9 @@ myTeam::myTeam(); vie(50), attaque1(10), attaque2(20);
 void getVie(){
     return vie;
 }
+<<<<<<< Updated upstream
+=======
+
+
+enemie::enemie(); vie(50), attaque1(10), attaque2(20);
+>>>>>>> Stashed changes
